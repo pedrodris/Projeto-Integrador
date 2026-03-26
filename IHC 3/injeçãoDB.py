41591406e0b945e43ce7c094c99e410fb7ddc776
Pedro Vinícius Rodrigues <148294050+pedrodris@users.noreply.github.com>
2026-03-26 07:34:47 -0300

    Add files via upload

---
+++ IHC 3/injeçãoDB.py
@@ -0,0 +1,16 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+try:
+    response = supabase.table("meals").select("*").execute()
+    print(response.data)
+except Exception as e:
+    print(e)
